29f65d0e952dec4484392b5b63dff5517ca6d272
1861100 <1861100@01-19833.pusd.dom>
2022-04-06 09:51:29 -0700

    push

---
+++ src/Miner.java
@@ -0,0 +1,76 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Miner{
+	
+	private int x, y; 
+	private Image img; 	
+	private AffineTransform tx;
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int newY) {
+		y = newY;
+	}
+	public void setX(int newX) {
+		x = newX;
+	}
+	
+
+	public Miner(int x, int y) {
+		img = getImage("miner.png"); //load the image for Tree
+		this.x = x;
+		this.y = y;
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(x, y);
+		
+
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(x, y);
+		update();
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, tx, null);
+		update();
+	}
+	
+	//update the picture variable location
+	private void update() {
+		tx.setToTranslation(x, y);
+		tx.scale(.2, .2);
+	}
+
+	private void init(double a, double b) { //initialize
+		tx.setToTranslation(a, b);
+		tx.scale(0.5, 0.5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Miner.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+}
---
+++ src/Runner.java
@@ -0,0 +1,166 @@
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+	/*	get an attack that destroys the closest square to the miner
+	 * 	get collision and jumping
+	 * 
+	 */
+
+public class Runner extends JPanel implements ActionListener, MouseListener, KeyListener {
+	Miner m = new Miner(100, 150);
+	Background b = new Background();
+	private int score = 0;
+	private boolean test = true;
+	
+	public void paint(Graphics g){
+		b.paint(g);
+		m.paint(g);
+		if(m.getY() <= 150) {
+			m.setY(150);
+		}
+		if(m.getX() <= 0) {
+			m.setX(0);
+		}
+		if(m.getX() >= 790) {
+			m.setX(790);
+		}
+		if(m.getY() >= 875) {
+			m.setY(875);
+		}
+		
+		Font f = new Font("Times New Roman", Font.BOLD, 50);
+		g.setFont(f);
+		g.drawString("Bitcoin Miner", 290, 50);
+		g.drawString(score+"", 800, 50);
+		Font e = new Font("Times New Roman", Font.BOLD, 20);
+		g.setFont(e);
+	}
+	
+	public static void main(String[] arg) {
+		Runner r = new Runner();
+	}
+	public Runner() {
+		JFrame f = new JFrame("Bitcoin Miner");
+		f.setSize(new Dimension(900, 1000));
+		f.setBackground(Color.white);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	
+	}
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+		if(arg0.getKeyCode() == 39) { //right
+			m.setX(m.getX()+10);
+		}
+		
+		if(arg0.getKeyCode() == 37) { //left
+			m.setX(m.getX()-10);
+		}
+		
+		if(arg0.getKeyCode() == 40) {
+			if(m.getY() >= 150) {
+				m.setY(m.getY()+10);
+			}
+		}
+		if(arg0.getKeyCode() == 38) {
+			m.setY(m.getY()-10);
+		}
+		if(m.getY() >= 155) {
+			if(arg0.getKeyCode() == 39) {
+				m.changePicture("minerdigR.png");
+			}
+		}
+		if(m.getY() >= 155) {
+			if(arg0.getKeyCode() == 37) {
+				m.changePicture("minerdigL.png");
+			}
+		}
+		if(m.getY() <= 160) {
+			m.changePicture("miner.png");
+			
+		}
+	}//e
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
